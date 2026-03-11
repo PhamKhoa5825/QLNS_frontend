8841d1349b38b1a31cce3381755f5c97bb83e790
lyfm8 <lndk2k5@gmail.com>
2026-03-11 13:57:47 +0700

    remake layout

--- app/src/main/java/com/example/myapplication/network/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/network/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     // Thay bằng IP máy chạy Spring Boot
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://10.0.2.2:8081/";
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
